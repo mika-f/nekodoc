cc9cff60d545a4479abf5f1f87d7f7622340c292
Natsune Mochizuki <me@natsuneko.cat>
2022-08-16 22:48:35 +0900

    feat(nekodoc): get routings

---
+++ packages/nekodoc/src/server/routes.ts
@@ -0,0 +1,75 @@
+import fs from "fs/promises";
+import path from "path";
+
+type RoutingOptions = {
+  contentDir: string;
+  cleanUrls?: boolean;
+  trailingSlash?: boolean;
+};
+
+const getFileNameWithoutExtension = (w: string): string => {
+  const url = path.parse(w);
+  if (url.dir === "") return url.name;
+  if (url.dir === "/") return `/${url.name}`;
+  return `${url.dir}/${url.name}`;
+};
+
+const getRecursiveGetFiles = async (dir: string): Promise<string[]> => {
+  const entries = await fs.readdir(dir, { withFileTypes: true });
+  const files: string[] = [];
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const entry of entries) {
+    if (entry.isDirectory()) {
+      // eslint-disable-next-line no-await-in-loop
+      const internalEntries = await getRecursiveGetFiles(
+        `${dir}/${entry.name}`
+      );
+      files.push(...internalEntries);
+    }
+
+    if (entry.isFile()) {
+      files.push(`${dir}/${entry.name}`);
+    }
+  }
+
+  return files;
+};
+
+const getRoutings = async (
+  options: RoutingOptions
+): Promise<Record<string, string>> => {
+  const routings: Record<string, string> = {};
+  const routes = await getRecursiveGetFiles(options.contentDir);
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const route of routes.map((w) =>
+    w.replace(`${options.contentDir}/`, "/")
+  )) {
+    let name = route;
+
+    // path/to/index.mdx? -> path/to/
+    if (/index\.mdx?$/.test(name)) {
+      name = path.dirname(name);
+
+      if (name !== "/" && options.trailingSlash) name = `${name}/`;
+    }
+
+    // path/to/hoge.mdx -> /path/to/hoge or /path/to/hoge/
+    if (/\.mdx?$/.test(name)) {
+      if (options.cleanUrls) {
+        name = getFileNameWithoutExtension(name);
+
+        if (options.trailingSlash) {
+          name = `${name}/`;
+        }
+      }
+    }
+
+    routings[name] = route;
+  }
+
+  return routings;
+};
+
+export default getRoutings;
