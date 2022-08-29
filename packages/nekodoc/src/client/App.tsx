30efb89f65781c88c2bb661f0f1509f487cbff4e
Natsune Mochizuki <me@natsuneko.cat>
2022-08-29 21:40:23 +0900

    fix(nekodoc): remove unused imports

--- packages/nekodoc/src/client/App.tsx
+++ packages/nekodoc/src/client/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import React, { useMemo } from "react";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 import type { MDXComponents } from "mdx/types";
